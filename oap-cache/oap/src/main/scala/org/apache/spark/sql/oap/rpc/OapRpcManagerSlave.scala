e2919aa78f9bfaf30f53552fefdc37b527baa230
offthewall123 <dingyu.xu@intel.com>
2020-08-28 09:48:56 +0800

    rpcEnv.stop(driverEndpoint)

--- oap-cache/oap/src/main/scala/org/apache/spark/sql/oap/rpc/OapRpcManagerSlave.scala
+++ oap-cache/oap/src/main/scala/org/apache/spark/sql/oap/rpc/OapRpcManagerSlave.scala
@@ -93,7 +93,7 @@ private[spark] class OapRpcManagerSlave(
 
   override private[spark] def stop(): Unit = {
     oapHeartbeater.shutdown()
-    rpcEnv.shutdown()
+    rpcEnv.stop(driverEndpoint)
   }
 }
 
