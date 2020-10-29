10a3ae2a5bf0d7dc709a08d19701176927ec1d28
offthewall123 <dingyu.xu@intel.com>
2020-10-29 13:38:21 +0800

    split shell to install and start

--- sbin/start_plasma_server.sh
+++ sbin/start_plasma_server.sh
@@ -1,29 +1,14 @@
-# install arrow and plasma
-cd $ROOT_DIR
-ARROW_REPO=https://github.com/Intel-bigdata/arrow.git
-ARROW_REPO_BRANCH=oap-master
-git clone -b $ARROW_REPO_BRANCH $ARROW_REPO arrow
-DIR=$ROOT_DIR/arrow
-cd $DIR/cpp
-rm -rf release
-mkdir release
-cd release
+#!/bin/bash
 
-# build libarrow, libplasma, libplasma_java
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-g -O3" -DCMAKE_CXX_FLAGS="-g -O3" -DARROW_BUILD_TESTS=on -DARROW_PLASMA_JAVA_CLIENT=on -DARROW_PLASMA=on -DARROW_DEPENDENCY_SOURCE=BUNDLED ..
-make -j40
-sudo make install -j40
+usage() {
+  echo "Usage: $0 please input size, mount point, socket"
+  echo "For example: ./start_plasma_server 350000000000 /mnt/pmem /tmp/plasmaStore"
+}
 
-# copy libs to lib path
-sudo rm /lib64/libarrow.so*
-sudo cp $DIR/cpp/release/release/libarrow.so.100.0.0 /lib64/
-sudo ln -s /lib64/libarrow.so.100.0.0 /lib64/libarrow.so.100
-sudo ln -s /lib64/libarrow.so.100 /lib64/libarrow.so
+if [ "$#" -ne 3 ]; then
+                usage
+                exit 0
+fi
 
-sudo rm /lib64/libplasma.so*
-sudo cp $DIR/cpp/release/release/libplasma.so.100.0.0 /lib64/
-sudo ln -s /lib64/libplasma.so.100.0.0 /lib64/libplasma.so.100
-
-sudo rm /lib64/libplasma_java.so*
-sudo cp $DIR/cpp/release/release/libplasma_java.so /lib64/
-sudo ldconfig
+rm $3
+plasma-store-server -m $1 -d $2 -s $3
--- sbin/start_redis_server.sh
+++ sbin/start_redis_server.sh
@@ -1,9 +1,2 @@
-# install redis
-mkdir redisInstall
-cd redisInstall
-wget https://download.redis.io/releases/redis-6.0.8.tar.gz
-tar xzf redis-6.0.8.tar.gz
-cd redis-6.0.8
-make
-
+#!/bin/bash
 ./src/redis-server --protected-mode no
